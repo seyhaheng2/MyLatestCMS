5d5070c50981741dfd298c2f20142eadb4d9472b
seyhaheng2 <seyhaheng2@gmail.com>
2017-05-16 13:38:41 +0700

    cms page dmca and privary contact done

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,13 +1,16 @@
 class PageController < ApplicationController
   def privacy
+    @sites = Site.last(1)
   end
 
   def contact
   end
 
   def dmca
+    @sites = Site.last(1)
   end
 
   def about
+    @sites = Site.last(1)
   end
 end
